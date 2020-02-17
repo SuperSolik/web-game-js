a4b076c402b243aa47cf24ecc7504d51923aab79
SuperSolik <serpetr99@gmail.com>
2020-02-17 19:11:58 +0300

    removed html garbage, raw game left

--- src/main.js
+++ src/main.js
@@ -5,27 +5,9 @@ let game = null;
 document.addEventListener("DOMContentLoaded", () => {
 	const canvas = document.getElementById("canvas");
 	const ctx = canvas.getContext("2d");
-	const scoreElement = document.getElementById("score");
-	const loginBtn = document.querySelector(".header button");
-	const screen = document.querySelector(".gameover");
+
+	const restartBtnScreen = document.querySelector(".gameover");
 	const restartBtn = document.querySelector(".gameover button");
-
-	function updateScore(score) {
-		scoreElement.innerText = score;
-		if (!localStorage.hasOwnProperty('scoreTable')) {
-			localStorage.setItem('scoreTable', '{}');
-		}
-
-		const table = JSON.parse(localStorage.getItem('scoreTable'));
-		table[localStorage.username] = score;
-		localStorage.setItem('scoreTable', JSON.stringify(table));
-	}
-
-	function onRestart() {
-		screen.classList.remove("show");
-		game = null;
-		start();
-	}
 
 	function start() {
 		ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -34,51 +16,27 @@ document.addEventListener("DOMContentLoaded", () => {
 			ctx: ctx,
 			canvas: canvas,
 			callbacks: {
-				updateScore: updateScore,
-				gameOver: gameOver
+				updateScore: (score) => console.log(`NEW SCORE IS ${score}`),
+				gameOver: () => {
+					console.log("GAME OVER");
+					restartBtnScreen.classList.add("show");
+				}
 			}
 		});
 		game.run();
 	}
 
-	function onLogin() {
-		let user = prompt("Input username");
-		localStorage.username = user;
-
-		start();
-	}
-
 	function gameOver(score, end = false) {
-		ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-		const title = document.querySelector(".gameover h1");
-
-		if (end) {
-			title.innerText = "End of the path!";
-		} else {
-			title.innerText = "You died...";
-		}
-
-		const tableBody = document.querySelector('#score-table tbody');
-		tableBody.innerHTML = "";
-		const scoreTableData = JSON.parse(localStorage.getItem('scoreTable'));
-
-
-		let markup = '';
-		for (let userName in scoreTableData) if (scoreTableData.hasOwnProperty(userName)) {
-			markup += `
-            <tr>
-              <td>${userName}</td>
-              <td>${scoreTableData[userName]}</td>
-            </tr>
-          `
-		}
-		tableBody.insertAdjacentHTML('beforeend', markup);
-		screen.classList.add("show");
+		restartBtnScreen.classList.add("show");
 	}
 
-	loginBtn.addEventListener("click", onLogin);
-	restartBtn.addEventListener("click", onRestart);
+	restartBtn.addEventListener("click", () => {
+		restartBtnScreen.classList.remove("show");
+			game = null;
+			start();
+	});
+
+	start();
 });
 
 
